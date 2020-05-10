01b3c3442a4d20a4bc7d542b281fc8a2b32af759
Cole Groff <groffcole@gmail.com>
2020-05-10 16:25:49 -0500

    tests over updating the resource server

--- auth/src/service-tokens/resource-server/events/Update.ts
+++ auth/src/service-tokens/resource-server/events/Update.ts
@@ -1,4 +1,29 @@
 import { CloudFormationCustomResourceUpdateEvent } from "aws-lambda/trigger/cloudformation-custom-resource";
+import { getAuth0ManagementClient } from "../../../utilities/Auth0Utility";
+import { sendCloudFormationResponse } from "../../../utilities/CloudFormationUtility";
 
-// @ts-ignore
-export const updateResourceServer = async (updateEvent: CloudFormationCustomResourceUpdateEvent) => {}
+export const updateResourceServer = async (updateEvent: CloudFormationCustomResourceUpdateEvent) => {
+  const managementClient = await getAuth0ManagementClient();
+  await managementClient.updateResourceServer(
+    {
+      id: updateEvent.PhysicalResourceId
+    },
+    {
+      scopes: updateEvent.ResourceProperties.Scopes
+    }
+  );
+
+  await sendCloudFormationResponse(
+    updateEvent.ResponseURL,
+    JSON.stringify({
+      Status: "SUCCESS",
+      RequestId: updateEvent.RequestId,
+      LogicalResourceId: updateEvent.LogicalResourceId,
+      StackId: updateEvent.StackId,
+      PhysicalResourceId: updateEvent.PhysicalResourceId,
+      Data: {
+        Identifier: updateEvent.ResourceProperties.Identifier
+      }
+    })
+  );
+};
