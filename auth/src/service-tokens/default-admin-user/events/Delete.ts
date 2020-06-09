4f6c75f8a0cc37e4d3461b5e0e882ea73d9ec16f
Cole Groff <groffcole@gmail.com>
2020-06-09 15:44:10 -0500

    refactor

--- auth/src/service-tokens/default-admin-user/events/Delete.ts
+++ auth/src/service-tokens/default-admin-user/events/Delete.ts
@@ -1,12 +1,11 @@
 import { sendCloudFormationResponse } from "../../../utilities/CloudFormationUtility";
 import { getAuth0ManagementClient } from "../../../utilities/Auth0Utility";
 import { CloudFormationCustomResourceDeleteEvent } from "aws-lambda/trigger/cloudformation-custom-resource";
+import { Stages } from "../../../domain/Stages";
 
 export const deleteDefaultAdminUser = async (deleteEvent: CloudFormationCustomResourceDeleteEvent) => {
-  if (deleteEvent.ResourceProperties.Stage === "dev" || deleteEvent.ResourceProperties.Stage === "prod") {
-  } else {
-    const managementClient = await getAuth0ManagementClient();
-    await managementClient.deleteUser({ id: deleteEvent.PhysicalResourceId });
+  if (defaultAdminUserShouldBeDeleted(deleteEvent)) {
+    deleteTheDefaultAdminUser(deleteEvent);
   }
 
   await sendCloudFormationResponse(
@@ -19,4 +18,13 @@ export const deleteDefaultAdminUser = async (deleteEvent: CloudFormationCustomRe
       PhysicalResourceId: deleteEvent.PhysicalResourceId
     })
   );
+};
+
+const defaultAdminUserShouldBeDeleted = (deleteEvent: CloudFormationCustomResourceDeleteEvent) => {
+  return deleteEvent.ResourceProperties.Stage !== Stages.DEVELOPMENT && deleteEvent.ResourceProperties.Stage !== Stages.PRODUCTION;
+};
+
+const deleteTheDefaultAdminUser = async (deleteEvent: CloudFormationCustomResourceDeleteEvent) => {
+  const managementClient = await getAuth0ManagementClient();
+  await managementClient.deleteUser({ id: deleteEvent.PhysicalResourceId });
 };
