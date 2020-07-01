9e5f7e84189dc0d8aa71067e59f68b23cb6d294d
Cole Groff <groffcole@gmail.com>
2020-06-30 22:46:44 -0500

    chakra stuff

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ThemeProvider, CSSReset, ColorModeProvider, useColorMode, Button, Skeleton, Box, theme, Spinner, Stack } from "@chakra-ui/core";
 
-function App() {
+export const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bgColor = { light: "green.500", dark: "red.200" };
+  const color = { light: "white", dark: "gray.800" };
+
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <CSSReset />
-      <div>hello, world.</div>
+      <ColorModeProvider>
+        <div>hello, world.</div>
+        <Box flex="1" mb={4} bg={bgColor[colorMode]} color={color[colorMode]}>
+          This box's style will change based on the color mode.
+        </Box>
+        <Button onClick={toggleColorMode}>Toggle {colorMode === "light" ? "Dark" : "Light"}</Button>
+        <div>
+          <Skeleton height="20px" my="10px" />
+          <Skeleton height="100px" my="10px" />
+          <Skeleton height="20px" my="10px" />
+        </div>
+        <Stack isInline spacing={4}>
+          <Spinner size="xs" />
+          <Spinner size="sm" />
+          <Spinner size="md" />
+          <Spinner size="lg" />
+          <Spinner size="xl" />
+        </Stack>
+      </ColorModeProvider>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
