c1ff9b751ef408f95d5d55b38a82798c9215da3b
Cole Groff <groffcole@gmail.com>
2020-06-30 22:13:45 -0500

    setting up chakra ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,28 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          hello, world. potato chips in the bag. water bottles for sale.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <CSSReset />
+      <div>hello, world.</div>
+    </ThemeProvider>
   );
 }
 
