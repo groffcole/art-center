b8742248804977d05bd7006a51f644933f7c050f
Cole Groff <groffcole@gmail.com>
2020-06-30 21:56:06 -0500

    checking in

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core";
 
 function App() {
   return (
-    <>
-      <div className="bg-purple-300 p-4">
-        <div className="bg-green-100 p-4">hello, world.</div>
-        <div className="bg-green-100 p-4">hello, world.</div>
-        <div className="bg-green-100 p-4">hello, world.</div>
-        <div className="bg-green-100 p-4">hello, world.</div>
-        <div className="bg-green-100 p-4">hello, world.</div>
-        <div className="bg-green-100 p-4">hello, world.</div>
-      </div>
-    </>
+    <ThemeProvider>
+      <CSSReset />
+      <ColorModeProvider>
+        <div className="bg-purple-300 p-4">
+          <div className="bg-green-100 p-4">hello, world.</div>
+          <div className="bg-green-100 p-4">hello, world.</div>
+          <div className="bg-green-100 p-4">hello, world.</div>
+          <div className="bg-green-100 p-4">hello, world.</div>
+          <div className="bg-green-100 p-4">hello, world.</div>
+          <div className="bg-green-100 p-4">hello, world.</div>
+        </div>
+      </ColorModeProvider>
+    </ThemeProvider>
   );
 }
 
